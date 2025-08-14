c430dd03d872df395562b998014b77aac59e67b0
oscarsidsa81 <ovilches@sid-sa.com>
2025-08-14 13:32:25 +0200

    Primer Commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'sid_sale_order_draft_crm_fields',
+    'version': '1.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Campos en vista tree y form relacionados con el CRM',
+    'description': 'Campos para la identificación de la oportunidad vinculada a los diferentes presupuestos',
+    'author': 'oscarsidsa81',
+    'depends': ['base','sale_management','crm'],
+    'data': [
+        'views/sid_sale_order_draft_crm_fields.xml'
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
