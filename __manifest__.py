5c418e0276d5ed514f61b2f9212c4d29c50ad61f
oscarsidsa81 <ovilches@sid-sa.com>
2025-08-14 13:49:42 +0200

    Primer Commit

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'summary': 'Campos en vista tree y form relacionados con el CRM',
     'description': 'Campos para la identificación de la oportunidad vinculada a los diferentes presupuestos',
     'author': 'oscarsidsa81',
-    'depends': ['base','sale_management','crm','oct_sale_extra_fields'],
+    'depends': ['base','sale_crm','sale_management','crm','oct_sale_extra_fields'],
     'data': [
         'views/sid_sale_order_draft_crm_fields.xml'
     ],
